724aaac5f4c021d554c8460dcbcf3ee2436f5b4b
Alex William <awilliam@cis-catering.com>
2024-04-13 23:56:06 +0200

    Edit Country

--- app/(root)/(home)/settings/general/treeview/country.tsx
+++ app/(root)/(home)/settings/general/treeview/country.tsx
@@ -1,3 +1,4 @@
+import { Pencil } from 'lucide-react';
 import React from 'react'
 
 interface ICoountry {
@@ -12,10 +13,11 @@ interface ICoountry {
 
 const CountryWidget = ({country} : {country : ICoountry} ) => {
   return (
-    <div className={`flex flex-col justify-between border rounded-[8px] p-2 bg-slate-300 cursor-pointer
-    hover:bg-sidebar-background min-h-[50px] text-gray-800 mt-3 mr-3`}>
-    <p>{country.name} ({country.project?.length})</p>
-    </div>
+<div className="group flex items-center justify-between border rounded-[8px] p-2 bg-slate-300 cursor-pointer min-h-[50px] text-gray-800 mt-3 mr-3 relative">
+  <p>{country.name} ({country.project?.length})</p>
+  <Pencil className="hidden group-hover:block h-5 w-5 text-gray-600 ml-5" />
+</div>
+
   )
 }
 
